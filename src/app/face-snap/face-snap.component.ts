11733ba4a453a402d13c8204ff59676335ebf6fb
zakhiel50 <lucrigaud50@gmail.com>
2023-09-20 10:41:17 +0200

    declaration of face-snap component properties at the top of its class and initialized in the ngOnInit() method. Inserting values ​​into the html of the snap-face component with string interpolation for the content except the image inserted using the binding attribute.

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-face-snap',
   templateUrl: './face-snap.component.html',
   styleUrls: ['./face-snap.component.scss']
 })
-export class FaceSnapComponent {
+export class FaceSnapComponent implements OnInit {
+title! : string;
+description! : string;
+createdDate! : Date;
+snaps! : number;
+imgUrl!: string; 
 
+ngOnInit() {
+  this.title = "Me :)";
+  this.description = "It's me, Okay !? 🤪."
+  this.createdDate = new Date();
+  this.snaps = 6;
+  this.imgUrl = "https://media.licdn.com/dms/image/D4E03AQHEts4hMpHr6A/profile-displayphoto-shrink_800_800/0/1683886850340?e=2147483647&v=beta&t=QMCmuBueDbOSpUTfCAfqOv-BEVBsDNjEXSRjNzXHcCo";
+
+}
 }
