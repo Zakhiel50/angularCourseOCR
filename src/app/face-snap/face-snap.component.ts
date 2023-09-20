1b2767f8f78c713c0518bd1d4f11cf56df2ad717
zakhiel50 <lucrigaud50@gmail.com>
2023-09-20 12:06:38 +0200

    Added a button to like or dislike. Modification of the onAddSnap() => onAddOrRemoveSnap() method to only be able to like once or unlike once.

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -10,14 +10,35 @@ title! : string;
 description! : string;
 createdDate! : Date;
 snaps! : number;
-imgUrl!: string; 
+imgUrl!: string;
+snapped! : boolean;
+btnContent! : string;
 
-ngOnInit() {
-  this.title = "Me :)";
-  this.description = "It's me, Okay !? 🤪."
-  this.createdDate = new Date();
-  this.snaps = 6;
-  this.imgUrl = "https://media.licdn.com/dms/image/D4E03AQHEts4hMpHr6A/profile-displayphoto-shrink_800_800/0/1683886850340?e=2147483647&v=beta&t=QMCmuBueDbOSpUTfCAfqOv-BEVBsDNjEXSRjNzXHcCo";
+  ngOnInit() {
+    this.title = "Me :)";
+    this.description = "It's me, Okay !? 🤪."
+    this.createdDate = new Date();
+    this.snaps = 6;
+    this.imgUrl = "https://media.licdn.com/dms/image/D4E03AQHEts4hMpHr6A/profile-displayphoto-shrink_800_800/0/1683886850340?e=2147483647&v=beta&t=QMCmuBueDbOSpUTfCAfqOv-BEVBsDNjEXSRjNzXHcCo";
+    this.snapped = false
+    this.btnContent = "Oh Snap !"
+  }
 
+  
+  onAddOrRemoveSnap() {
+    if (this.snapped === false) {
+      this.snapped = true 
+      this.snaps++;
+      this.btnContent = "Oops unsnap !"  
+    }
+    else {
+      this.snapped = false;
+      this.snaps--
+      this.btnContent = "Oh Snap !"
+    }
+  }
+  
 }
-}
+
+
+
